3129d3b5868486ba291440d159c1175d17e24fdc
leonardomarciano <leonardo@marciano.io>
2018-07-16 14:42:04 -0300

    Configuração ok

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,23 @@ const ts = require('gulp-typescript')
 
 const tsProject = ts.createProject('tsconfig.json')
 
-// TODO: compile with tsconfig.json
+// TODO: compile with tsconfig.json ->
 gulp.task('scripts',['static'],  () => {
     const tsResult = tsProject.src().pipe(tsProject());
     return tsResult.js.pipe(gulp.dest('dist'));
 });
-// TODO: move statics file to dist dir
+
+// TODO: move statics file to dist dir ->
 gulp.task('static', ['clean'] , () => gulp.src(['src/**/*.json']).pipe(gulp.dest('dist')) );
-// TODO: clear dist dir 
+
+// TODO: clear dist dir -> 
 gulp.task('clean', () => gulp.src('dist').pipe(clean()) );
-// TODO: build sequence
-gulp.task('build', ['clean', 'static', 'scripts'])
+
+// TODO: build sequence -> clean, static, scripts
+gulp.task('build', ['scripts'])
+
+// TODO: Autoreload ->
+gulp.task('watch', ['build'], () => gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']))
+
+// ? Should default task reload the app?
+gulp.task('default', ['watch'])

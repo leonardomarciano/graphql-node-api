84d5fa7f8182579f74f01be21377039d481df6ad
leonardomarciano <leonardo@marciano.io>
2018-07-16 15:25:46 -0300

    express intro

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,17 @@
-console.log("Oi");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        this.express.use('/leroy', (req, res, next) => {
+            res.send({
+                hello: "Mundo"
+            });
+        });
+    }
+}
+exports.default = new App().express;

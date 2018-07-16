9581e9c2768ed1062a02332f136389743844aa10
leonardomarciano <leonardo@marciano.io>
2018-07-16 15:32:44 -0300

    utils and express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as http from 'http'
 import app from './app'
+import { normalizePort } from './utils/utils';
 
+// ? Declarative
 const server = http.createServer(app)
+const port = normalizePort(process.env.port || 3000)
 
-
-server.listen(3000);
-server.on('listening', ()=> console.log('Listening on port 3000'))
+// * Server
+server.listen(port);
+server.on('listening', ()=> console.log(`Listening on port ${port}`))
